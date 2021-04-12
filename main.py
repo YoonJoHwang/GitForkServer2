40851ef703fb0e751b989dc2f3e937d6c04c601f
So-yeon Park <kay991213@gmail.com>
2021-04-12 18:00:31 +0900

    Add Hello World 3

--- main.py
+++ main.py
@@ -3,8 +3,13 @@
 #
 print("Hello World 1 in team leader server")
 
+<<<<<<< HEAD
 print("Hello World 2 in team leader server")
 
 print("Hello World 3 in team member B local server")
 
 print("Hello World 3 in team member A local server")
+=======
+print("Hello World 3 in team member A local server")
+
+>>>>>>> Add Hello World 3
